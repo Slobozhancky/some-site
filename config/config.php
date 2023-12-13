bbfcb9d15c7dab7abf34832d9a3a8e3849046a7d
Slobozhancky <il.slob93@gmail.com>
2023-12-13 15:06:28 +0200

    added .gitignore, some settings for composer and constants

--- config/config.php
+++ config/config.php
@@ -1,3 +1,17 @@
 <?php
 
-define();
+define("DIR_PATH", dirname(__DIR__));
+define("DEBUG", 1);
+define("WWW", DIR_PATH . '/public');
+define("APP", DIR_PATH . '/app');
+define("CORE", DIR_PATH . '/vendor/core');
+define("HELPERS", DIR_PATH . '/vendor/core/helpers');
+define("CACHE", DIR_PATH . '/temp/cache');
+define("LOGS", DIR_PATH . '/temp/logs');
+define("CONFIG", DIR_PATH . '/config');
+define("LAYOUT", 'some-site');
+define("PATH", 'http://some-site');
+define("ADMIN", 'http://some-site/admin');
+define("NO-IMAGE", 'uploads/no-image.jpg');
+
+require_once DIR_PATH . '/vendor/autoload.php';
--- public/index.php
+++ public/index.php
@@ -1,3 +1,5 @@
 <?php
 
+require_once dirname(__DIR__) . '/config/config.php';
 
+d(123);
